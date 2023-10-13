eb36f50d5c6f9cf8b00b154f1067d01cd828f390
Eugenio Tampieri <eugenio@eutampieri.eu>
2023-10-13 22:55:27 +0200

    Moved filling to a class

--- app/src/main/kotlin/pdf_filler/App.kt
+++ app/src/main/kotlin/pdf_filler/App.kt
@@ -4,28 +4,13 @@
 package pdf_filler
 
 import com.gitlab.pdftk_java.com.lowagie.text.pdf.PdfReader
-import com.gitlab.pdftk_java.com.lowagie.text.pdf.PdfStamperImp
 import com.gitlab.pdftk_java.com.lowagie.text.pdf.XfdfReader
 import java.nio.file.Files
 import java.nio.file.Paths
-
-class App {
-    val greeting: String
-        get() {
-            return "Hello World!"
-        }
-}
 
 fun main() {
     val reader = PdfReader("file.pdf")
     val form = XfdfReader("file.xfdf")
     val outputStream = Files.newOutputStream(Paths.get("/tmp/test.pdf"))
-
-    // Code from com.gitlab.pdftk_java.filter
-    val writer = PdfStamperImp(reader, outputStream, '\u0000', false /* append mode */)
-    val fields = writer.getAcroFields()
-    writer.setFormFlattening(true)
-    fields.setGenerateAppearances(true)
-    fields.setFields(form)
-    writer.close()
+    PdfFiller(reader).fill(form, outputStream)
 }
